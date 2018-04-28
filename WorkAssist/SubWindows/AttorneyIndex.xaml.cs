2b41aa436032f44c67be17b92c10ddf2e5719c9d
jiayushd <38585419+jiayushd@users.noreply.github.com>
2018-04-29 06:59:37 +0800

    分组指标
    按分组（例如分公司、组）等获取个人指标列表，并进行展示。

--- WorkAssist/SubWindows/AttorneyIndex.xaml.cs
+++ WorkAssist/SubWindows/AttorneyIndex.xaml.cs
@@ -23,39 +23,39 @@ namespace WorkAssist.SubWindows
     {
         DataSet ds = new DataSet();
         AttorneyStatus ats=new  AttorneyStatus();
+        //ViewModel.AttorneyIndex ai;
         public AttorneyIndex()
         {
             InitializeComponent();
+            
+            dpStart.SelectedDate = DateTime.Now.Date.AddDays(-DateTime.Now.Date.Day + 1);
+            dpEnd.SelectedDate = DateTime.Now.Date;
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            dpStart.DisplayDate = DateTime.Now.Date.AddDays(-DateTime.Now.Date.Day + 1);
-            dpEnd.DisplayDate = DateTime.Now.Date;
-
+            
             DAL.TaskInfo taskInfos = new DAL.TaskInfo();
             ds = taskInfos.GetAll();
+            
             //spStatus.DataContext = ats;
 
             DAL.DepartmentMember members = new DAL.DepartmentMember();
-            List<MemberTree> department = new List<MemberTree> (){ GetAllMembers(members.GetAll())};
+            DataSet dsmem = members.GetAll();
+            //MessageBox.Show(dsmem.Tables[0].Rows[0]["在职状态"].ToString());
+            List<MemberTree> department = new List<MemberTree> (){ GetAllMembers(dsmem) };
             memberTree.ItemsSource = department;
             
         }
 
-        private void BtnAttorney_Click(object sender, RoutedEventArgs e)
-        {
-            //获取代理人状态数据保存在ats对象实例中
-            ats= new AttorneyStatus(ds,"舒丁");
-            spStatus.DataContext = ats;
-        }
 
-        private void TblkAllOA_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            detail.ItemsSource = ConvertDStoDTLS(ats.DsAllOA);
+        #region 私有方法
 
-        }
-
+        /// <summary>
+        /// 将ds转换为案件详情taskdetails
+        /// </summary>
+        /// <param name="ds"></param>
+        /// <returns></returns>
         private List<TaskDetails> ConvertDStoDTLS(DataSet ds)
         {
             //新建摘要对象
@@ -82,7 +82,7 @@ namespace WorkAssist.SubWindows
 
             foreach (DataRow dr in ds.Tables[0].Rows)
             {
-                if (dr["部门"].ToString()=="电子部")
+                if (dr["部门"].ToString()=="电子部" && dr["在职状态"].ToString()!="False")
                 {
                 //新建一个与datarow对应的子树
                 MemberTree Company = new MemberTree();
@@ -104,25 +104,122 @@ namespace WorkAssist.SubWindows
             return Department;                
         }
 
-        private void MemberTree_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        /// <summary>
+        /// 获取某个节点上的所有叶子节点，也即所有的代理人
+        /// </summary>
+        /// <param name="mt"></param>
+        /// <returns></returns>
+        private List<MemberTree> GetAllLeafofOneNode(MemberTree mt)
         {
+            List<MemberTree> leafs = new List<MemberTree>();
 
-            MemberTree mt = new MemberTree();
-            mt = (MemberTree)memberTree.SelectedItem;
-
-            if (mt.Children==null)
+            if (mt.Children!=null)
             {
-                DataRow[] drs= ds.Tables[0].Select("承办人 LIKE '"+mt.Name+"'");
-                DataSet dstemp = new DataSet();
-                dstemp = ds.Clone();
-                foreach (DataRow dr in drs)
+                foreach (MemberTree mtchild in mt.Children)
                 {
-                    dstemp.Tables[0].Rows.Add(dr.ItemArray);
+                    foreach (MemberTree mtleaf in GetAllLeafofOneNode(mtchild))
+                    {
+                        leafs.Add(mtleaf);
+                    }
                 }
-                ViewModel. AttorneyIndex ai = new ViewModel.AttorneyIndex(dstemp,dpStart.DisplayDate.Date,dpEnd.DisplayDate.Date);
-                spIndex.DataContext = ai;
+
+            }
+            else
+            {
+                leafs.Add(mt);
             }
 
+            return leafs;
         }
+
+        /// <summary>
+        /// 如果是叶子节点，则获取指标详情
+        /// </summary>
+        /// <param name="mt"></param>
+        /// <returns></returns>
+        private ViewModel.AttorneyIndex GetAttorneyIndexofOne(MemberTree mt)
+        {
+            DataRow[] drs = ds.Tables[0].Select("承办人 LIKE '" + mt.Name + "'");
+            DataSet dstemp = new DataSet();
+            dstemp = ds.Clone();
+            foreach (DataRow dr in drs)
+            {
+                dstemp.Tables[0].Rows.Add(dr.ItemArray);
+            }
+            ViewModel.AttorneyIndex aii = new ViewModel.AttorneyIndex(dstemp, dpStart.SelectedDate, dpEnd.SelectedDate);
+            aii.Name = mt.Name;
+            aii.PercentofExceed= Math.Round(aii.PercentofExceed, 3);
+            return aii;
+        }
+        #endregion
+
+
+        /// <summary>
+        /// 点击节点的处理逻辑：获取该节点下的所有人名，并生成指标数据填入表中。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MemberTree_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            List<MemberTree> leafs = new List<MemberTree>();//创建叶子列表用于保存所有的叶子节点
+            MemberTree mt = new MemberTree();//创建成员树对象，用于保存所选择的节点数实例
+            List<ViewModel.AttorneyIndex> indices = new List<ViewModel.AttorneyIndex>();//创建指标详情列表，用于保存所选择的节点下的所有叶子节点的指标详情
+
+            mt = (MemberTree)memberTree.SelectedItem;
+            leafs = GetAllLeafofOneNode(mt);
+
+            foreach (MemberTree leaf in leafs)
+            {
+                indices.Add(GetAttorneyIndexofOne(leaf));
+            }
+
+            dgDetail.ItemsSource = indices;
+
+            //MemberTree mt = new MemberTree();
+            //mt = (MemberTree)memberTree.SelectedItem;
+
+            //if (mt.Children==null)
+            //{
+            //    DataRow[] drs= ds.Tables[0].Select("承办人 LIKE '"+mt.Name+"'");
+            //    DataSet dstemp = new DataSet();
+            //    dstemp = ds.Clone();
+            //    foreach (DataRow dr in drs)
+            //    {
+            //        dstemp.Tables[0].Rows.Add(dr.ItemArray);
+            //    }
+            //    ai = new ViewModel.AttorneyIndex(dstemp,dpStart.SelectedDate,dpEnd.SelectedDate);
+            //    spIndex.DataContext = ai;
+        }
+
+    
+
+        #region 个人案件状态区各数字点击后展示相关的列表
+        private void TblkAllOA_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            detail.ItemsSource = ConvertDStoDTLS(ats.DsAllOA);
+
+        }
+
+        #endregion
+
+        #region 指标详情区各数字点击后展示相关的列表
+        private void TbNumofHandled_MouseDown(object sender, MouseButtonEventArgs e)
+            {
+
+                //TaskList tl = new TaskList(ai.TdsHandled);
+                //tl.Show();
+
+            }
+
+        private void TbNumofExceedlimit_MouseDown(object sender, MouseButtonEventArgs e)
+            {
+
+                //TaskList tl = new TaskList(ai.TdsExceedlimit);
+                //tl.Show();
+            }
+
+        #endregion
+
+
     }
 }
--- WorkAssist/ViewModel/AttorneyIndex.cs
+++ WorkAssist/ViewModel/AttorneyIndex.cs
@@ -11,26 +11,43 @@ namespace WorkAssist.ViewModel
     {
         string name;
         double firstVirsionWeight;//初稿权值
+        List<TaskDetails> tdsFirstVirsions;//初稿详情列表
         double doneWeight;//递交权值
-        DateTime startDate;//计算区间起始日期
-        DateTime endDate;//计算区间结束日期
+        List<TaskDetails> tdsDone;//递交详情列表
+        DateTime? startDate;//计算区间起始日期
+        DateTime? endDate;//计算区间结束日期
         int numofDoneOA;//递交OA的数量
+        List<TaskDetails> tdsDoneOA;//递交OA详情列表
         int numofHandled;//经手新申请案件数量
+        List<TaskDetails> tdsHandled;//经手新申请详情列表
         int numofExceedlimit;//超期案件数量
+        List<TaskDetails> tdsExceedlimit;//经手新申请详情列表
+        double percentofExceed;
         int daysofExceedlimit;//超期天数
+        int numofEntrusted;//委托案件量
+        List<TaskDetails> tdsEntrusted;//经手新申请详情列表
 
         public double FirstVirsionWeight { get => firstVirsionWeight; set => firstVirsionWeight = value; }
         public double DoneWeight { get => doneWeight; set => doneWeight = value; }
-        public DateTime StartDate { get => startDate; set => startDate = value; }
-        public DateTime EndDate { get => endDate; set => endDate = value; }
+        public DateTime? StartDate { get => startDate; set => startDate = value; }
+        public DateTime? EndDate { get => endDate; set => endDate = value; }
         public int NumofDoneOA { get => numofDoneOA; set => numofDoneOA = value; }
         public int NumofHandled { get => numofHandled; set => numofHandled = value; }
         public int NumofExceedlimit { get => numofExceedlimit; set => numofExceedlimit = value; }
         public int DaysofExceedlimit { get => daysofExceedlimit; set => daysofExceedlimit = value; }
         public string Name { get => name; set => name = value; }
+        public double PercentofExceed { get => percentofExceed; set => percentofExceed = value; }
+        public List<TaskDetails> TdsFirstVirsions { get => tdsFirstVirsions; set => tdsFirstVirsions = value; }
+        public List<TaskDetails> TdsDone { get => tdsDone; set => tdsDone = value; }
+        public List<TaskDetails> TdsDoneOA { get => tdsDoneOA; set => tdsDoneOA = value; }
+        public List<TaskDetails> TdsHandled { get => tdsHandled; set => tdsHandled = value; }
+        public List<TaskDetails> TdsExceedlimit { get => tdsExceedlimit; set => tdsExceedlimit = value; }
+        public int NumofEntrusted { get => numofEntrusted; set => numofEntrusted = value; }
+        public List<TaskDetails> TdsEntrusted { get => tdsEntrusted; set => tdsEntrusted = value; }
 
-        public AttorneyIndex(DataSet ds, DateTime startdate, DateTime enddate)
+        public AttorneyIndex(DataSet ds, DateTime? startdate, DateTime? enddate)
         {
+
             startDate = startdate;
             endDate = enddate;
             firstVirsionWeight = 0;
@@ -39,16 +56,25 @@ namespace WorkAssist.ViewModel
             numofHandled = 0;
             numofExceedlimit = 0;
             daysofExceedlimit = 0;
+            numofEntrusted = 0;
+            tdsFirstVirsions = new List<TaskDetails>();
+            tdsDone = new List<TaskDetails>();
+            tdsDoneOA= new List<TaskDetails>();
+            tdsHandled = new List<TaskDetails>();
+            tdsExceedlimit = new List<TaskDetails>();
+            tdsEntrusted= new List<TaskDetails>();
 
             //根据起始日期和结束日期计算区间段内的各种指标
 
             List<TaskDetails> tds = ConvertDStoDTLS(ds);
+
             foreach (TaskDetails td in tds)
             {
                 //初稿权值累加
                 if (td.FirstVirsionDate>=startDate && td.FirstVirsionDate<=endDate)
                 {
                     firstVirsionWeight = firstVirsionWeight + td.Weight;
+                    tdsFirstVirsions.Add(td);
                     if (td.TaskName == "OA答复")
                     {
                         numofDoneOA = numofDoneOA + 1;
@@ -58,27 +84,57 @@ namespace WorkAssist.ViewModel
                 if (td.DoneDate >= startDate && td.DoneDate <= endDate)
                 {
                     doneWeight = doneWeight + td.Weight;
+                    tdsDone.Add(td);
 
                 }
                 //经手新申请数量累加
-                if (td.TaskName=="新申请")
+                if (td.TaskName=="新申请" && ((td.FirstVirsionDate >= startDate && td.FirstVirsionDate <= endDate)||(td.DoneDate >= startDate && td.DoneDate <= endDate)||td.DoneDate.ToString()==""))
                 {
                     numofHandled = numofHandled + 1;
-                }
-                //超期新申请数量及超期天数累加：完成了初稿，但记录超期
-                if (td.TaskName == "新申请" && td.FirstVirsionDate>td.FirstVirsionDeadlineOutside)
-                {
-                    numofExceedlimit = numofExceedlimit + 1;
-                    daysofExceedlimit = (td.FirstVirsionDate - td.FirstVirsionDeadlineOutside).Value.Days;
+                    tdsHandled.Add(td);
+
+                    //超期新申请数量及超期天数累加：完成了初稿，但记录超期
+                    if ((td.FirstVirsionDeadlineOutside != null && td.FirstVirsionDate>td.FirstVirsionDeadlineOutside) || td.FirstVirsionDate > td.FirstVirsionDeadlineInternal)
+                    {
+                        numofExceedlimit = numofExceedlimit + 1;
+                        if (td.FirstVirsionDeadlineOutside!=null)
+                        {
+                            daysofExceedlimit += (td.FirstVirsionDate - td.FirstVirsionDeadlineOutside).Value.Days;
+                        }
+                        else
+                        {
+                            daysofExceedlimit += (td.FirstVirsionDate - td.FirstVirsionDeadlineInternal).Value.Days;
+                        }
+                        tdsExceedlimit.Add(td);
+                    }
+
+                    //超期新申请数量累加：未完成初稿，但初稿期限已过。
+                    if (td.FirstVirsionDate==null && (td.FirstVirsionDeadlineOutside != null && td.FirstVirsionDeadlineOutside < DateTime.Now.Date || td.FirstVirsionDeadlineInternal < DateTime.Now.Date))
+                    {
+                        numofExceedlimit = numofExceedlimit + 1;
+                        if (td.FirstVirsionDeadlineOutside != null)
+                        {
+                            daysofExceedlimit += (DateTime.Now.Date - td.FirstVirsionDeadlineOutside).Value.Days;
+                        }
+                        else
+                        {
+                            daysofExceedlimit += (DateTime.Now.Date - td.FirstVirsionDeadlineInternal).Value.Days;
+                        }
+                        tdsExceedlimit.Add(td);
+                    }
                 }
 
-                //超期新申请数量累加：未完成初稿，但初稿期限已过。
-                if (td.TaskName == "新申请" && td.FirstVirsionDeadlineOutside < DateTime.Now.Date)
+                //委托案件累加
+                if (td.EntrustDate >= startDate && td.EntrustDate <= endDate)
                 {
-                    numofExceedlimit = numofExceedlimit + 1;
-                    daysofExceedlimit = (DateTime.Now.Date-td.FirstVirsionDeadlineOutside).Value.Days;
+                    numofEntrusted += 1;
+                    tdsEntrusted.Add(td);
                 }
+
             }
+
+            PercentofExceed =(double) numofExceedlimit / numofHandled;
+            //PercentofExceed = Math.Round(PercentofExceed,2);
 
         }
 
